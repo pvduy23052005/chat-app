8c269a26b288627728974071a188098c3e8f7ec3
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-12-23 11:20:12 +0700

    config express , pug , typescript

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import path from "path";
+
+const port: number | string | undefined = process.env.PORT;
+const app: Express = express();
+
+// config pug . 
+app.set('views', path.join(__dirname, "src", 'views'));
+app.set('view engine', 'pug');
+
+// config public  . 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("ok");
+})
+
+app.listen(port, () => {
+  console.log(`Server listening: ${port}`);
+})
