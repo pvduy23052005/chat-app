1680e4611175056b80db4dac61e8f4e91dab7888
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-12-23 20:56:56 +0700

    Add : cấu hình socket.io

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,38 @@ dotenv.config();
 import path from "path";
 import * as database from "./config/database";
 import indexRoute from "./routes/index.route";
+import { Server, Socket } from "socket.io";
+import http from "http"
+
 
 const port: number | string | undefined = process.env.PORT;
 const app: Express = express();
 
+// congif socket.io
+const server = http.createServer(app);
+const io = new Server(server, {
+  // Cấu hình CORS để tránh lỗi nếu client/server khác domain (dev mode)
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+io.on("connection", (socket: Socket) => {
+  console.log(socket.id);
+})
+
 // config pug . 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// config public  . 
 app.use(express.static(path.join(__dirname, "public")));
+
+// config global . 
+(global as any)._io = io;
 
 database.connectDatabase();
 
 indexRoute(app);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening: ${port}`);
 })
